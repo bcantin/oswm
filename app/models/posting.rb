8f658567d60ef69af95268d427fffb926525fdc3
Brad <brad.cantin@gmail.com>
2011-03-05 14:35:45 -0500

    added a default language of NA if our user did not select one

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -10,11 +10,13 @@
 #
 
 class Posting < ActiveRecord::Base
-      
-  validates :idea, :presence => true
   
   has_many :posting_languages
   has_many :languages, :through => :posting_languages
+  
+  after_save :set_n_a_language
+  
+  validates :idea, :presence => true
   
   def self.fresh
     where('created_at >= ?', 30.minutes.ago).order('created_at')
@@ -28,4 +30,13 @@ class Posting < ActiveRecord::Base
     display_name.present? ? display_name : 'Somebody'
   end
   
+  private
+    def set_n_a_language
+      return true if languages.present?
+      unless l = Language.where(:name => 'N/A').first
+        l = Language.create(:name => 'N/A')
+      end
+      languages << l
+    end
+    
 end
