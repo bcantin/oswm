55d66e56081a5cd6a71e9ca6ebd9a50ea21fb836
Brad <brad.cantin@gmail.com>
2011-03-05 09:15:03 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+
+# gem 'pg'
+gem 'sqlite3'
+
+gem "haml", ">= 3.0.0"
+gem "haml-rails"
+
+group :development, :test do
+  gem "rspec-rails", ">= 2.0.1" 
+  gem "cucumber-rails" 
+  gem "capybara"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oswm::Application.config.secret_token = 'c0be92c833ddeed5121754081994648ab368aaf1fadfca742af735623a3644bd8de952624327c370d9e95a38b105962209ed885e6ac65c626ff78d1d975d958a'
