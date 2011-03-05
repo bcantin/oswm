095b41efcd611d00f6cd437c28b365c73b3555b7
Brad <brad.cantin@gmail.com>
2011-03-05 16:40:50 -0500

    updated production.rb to send mail using heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,17 @@ Oswm::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  
+  
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
   
   # Enable threaded mode
   # config.threadsafe!
